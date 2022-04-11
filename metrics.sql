b86183d58cb1cdd2c73e51901a4b7710a210dd3b
annchursina <102875063+annchursina@users.noreply.github.com>
2022-04-11 12:43:25 +0300

    Add files via upload

--- metrics.sql
+++ metrics.sql
@@ -1,23 +1,23 @@
 ---Total Sales---
 
-select date(order_date) as Дата, sum(sales) as Total_Sales from orders 
+select date(order_date) as Р”Р°С‚Р°, sum(sales) as Total_Sales from orders 
 group by date(order_date)
 order by 1;
 
 ---Total Profit---
 
-select date(order_date) as Дата, sum(profit) as Total_Profit from orders 
+select date(order_date) as Р”Р°С‚Р°, sum(profit) as Total_Profit from orders 
 group by date(order_date)
 order by 1;
 
 
 ---Profit Ratio---
-select date(order_date) as Дата, sum(profit)/sum(sales) as Profit_Ratio from orders 
+select date(order_date) as Р”Р°С‚Р°, sum(profit)/sum(sales) as Profit_Ratio from orders 
 group by date(order_date)
 order by 1;
 
 ---Profit per Order---
-select date(order_date) as Дата,
+select date(order_date) as Р”Р°С‚Р°,
 sum(profit)/count(distinct order_id) as Profit_per_order from orders 
 group by 1
 order by 1;
@@ -30,36 +30,36 @@ group by 1,2
 order by 3;
 
 ---Avg. Discount---
-select date(order_date) as Дата, avg(discount) as average_discount from orders
+select date(order_date) as Р”Р°С‚Р°, avg(discount) as average_discount from orders
 group by 1
 order by 1;
 
 ---Monthly Sales by Segment---
-select date_part('year', order_date) as Год, date_part('month', order_date) as Месяц, segment, sum(sales) as Продажи from orders
+select date_part('year', order_date) as Р“РѕРґ, date_part('month', order_date) as РњРµСЃСЏС†, segment, sum(sales) as РџСЂРѕРґР°Р¶Рё from orders
 group by 1,2,3
 order by 1, 2;
 
 ---Monthly Sales by Product Category---
-select date_part('year', order_date) as Год, date_part('month', order_date) as Месяц, category, sum(sales) as Продажи from orders
+select date_part('year', order_date) as Р“РѕРґ, date_part('month', order_date) as РњРµСЃСЏС†, category, sum(sales) as РџСЂРѕРґР°Р¶Рё from orders
 group by 1,2,3
 order by 1, 2;
 
 ---Sales and Profit by Customer---
-select customer_id, customer_name, sum(sales) as Сумма_продаж, sum(profit) as Сумма_прибыли from orders o
+select customer_id, customer_name, sum(sales) as РЎСѓРјРјР°_РїСЂРѕРґР°Р¶, sum(profit) as РЎСѓРјРјР°_РїСЂРёР±С‹Р»Рё from orders o
 group by 1,2
 order by 3,4;
 
 ---Customer Ranking---
-select customer_id, customer_name, sum(sales) as Сумма_продаж, sum(profit) as Сумма_прибыли from orders o
+select customer_id, customer_name, sum(sales) as РЎСѓРјРјР°_РїСЂРѕРґР°Р¶, sum(profit) as РЎСѓРјРјР°_РїСЂРёР±С‹Р»Рё from orders o
 group by 1,2
 order by 3,4;
 
 ---Sales per region---
-select date(order_date) as Дата, region, sum(sales) from orders
+select date(order_date) as Р”Р°С‚Р°, region, sum(sales) from orders
 group by 1,2
 order by 1,2;
 
 ---Sales per Manager---
-select date_part('year', order_date) as Год, date_part('month', order_date) as Месяц, person, sum(sales) from
+select date_part('year', order_date) as Р“РѕРґ, date_part('month', order_date) as РњРµСЃСЏС†, person, sum(sales) from
 orders inner join people on orders.region=people.region
 group by 1,2,3;
